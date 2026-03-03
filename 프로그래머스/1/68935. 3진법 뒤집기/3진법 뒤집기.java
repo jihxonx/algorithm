735f76b209c9effd7aa785b34ee7abf0792e42e6
jihxonx <git4ub0@gmail.com>
2026-03-03 10:46:18 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0){
+            sb.append(n % 3);
+            n /= 3;
+        }
+        
+        int answer = 0;
+        int power = 1;
+        
+        for(int i = sb.length() - 1; i >=0; i--){
+            answer += (sb.charAt(i) - '0') * power;
+            power *= 3;
+        }
+        
+        return answer;
+    }
+}
