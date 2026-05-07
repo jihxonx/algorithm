ac8cb64b634f9140c07a62109540328ad4ef7427
jihoon <git4ub0@gmail.com>
2026-05-07 10:27:59 +0900

    [level 1] Title: 문자열 나누기, Time: 11.04 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int same = 0;
+        int diff = 0;
+        char x = ' ';
+        
+        for(int i = 0; i < s.length(); i++){
+            if(same == 0 && diff == 0){
+                x = s.charAt(i);
+            }
+            
+            if(s.charAt(i) == x){
+                same++;
+            }else{
+                diff++;
+            }
+            
+            if(same == diff){
+                answer++;
+                same = 0;
+                diff = 0;
+            }
+        }
+        
+        if(same != 0 || diff !=0){
+            answer++;
+        }
+        
+        return answer;
+    }
+}
